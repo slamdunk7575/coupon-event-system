a0455361c4eac2f89b6d603c5378a40558eb9a8e
slamdunk7575 <slamdunk7575@gmail.com>
2023-11-30 17:41:48 +0900

    (feat) init commit

---
+++ api/src/main/java/me/test/coupon/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package me.test.coupon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
