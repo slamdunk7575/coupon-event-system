68dc746f8f9a1febf7af7cfad0c510b05e81bf27
slamdunk7575 <slamdunk7575@gmail.com>
2023-12-01 11:34:47 +0900

    (feat) 여러명이 쿠폰 시스템에 응모할때 문제점 확인 (Race Condition 발생)

--- api/src/test/java/me/test/coupon/api/service/CouponApplyServiceTest.java
+++ api/src/test/java/me/test/coupon/api/service/CouponApplyServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -18,7 +22,7 @@ class CouponApplyServiceTest {
     private CouponRepository couponRepository;
 
 
-    @DisplayName("이벤트에 한번 응모하고 쿠폰이 발급되는 것을 확인한다.")
+    @DisplayName("한명이 이벤트에 응모하고 쿠폰이 발급되는 것을 확인한다.")
     @Test
     void apply_one_coupon() {
         // given
@@ -30,5 +34,35 @@ class CouponApplyServiceTest {
 
         // then
         assertThat(count).isEqualTo(1);
+    }
+
+
+    // 문제점
+    // Race Condition: 두개 이상의 쓰레드(여러명)가 공유자원(쿠폰)에 access 하고 동시에 작업을 수행하려고 할때 발생함
+    @DisplayName("여러명이 이벤트에 응모하고 쿠폰이 여러장 발급되는 것을 확인한다.")
+    @Test
+    void apply_multiple_coupon() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    couponApplyService.apply(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        long couponCount = couponRepository.count();
+
+        // then
+        assertThat(couponCount).isEqualTo(100);
     }
 }
